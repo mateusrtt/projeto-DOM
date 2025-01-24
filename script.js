37599ca922c8f0c3855afcecf538bb718a0eef63
mateusrtt <mateuseduardo779@gmail.com>
2025-01-24 14:55:45 -0300

    Adicionei interatividade ao accordion com animações CSS e lógica de exibição no javascript

--- script.js
+++ script.js
@@ -21,13 +21,22 @@ if(tabMenu.length && tabContent.length) {
 }
 initTabNav();
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
+function initAccordion(){
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const activeClass = 'ativo';
+    
+    if(accordionList.length){
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-function activeAccordion(){
-    this.classList.add('ativo');
-    this.nextElementSibling.classList.add('ativo');
+    function activeAccordion(){
+        this.classList.toggle(activeClass);
+        this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+        item.addEventListener('click', activeAccordion);
+    });
+  }
 }
-
-accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-})
+initAccordion();
