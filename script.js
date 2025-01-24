6e9fb81b3dc28785c12219869393d21ad48963b0
mateusrtt <mateuseduardo779@gmail.com>
2025-01-24 14:31:47 -0300

    Adicionei evento de clique para ativação de itens no accordion

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ if(tabMenu.length && tabContent.length) {
 }
 initTabNav();
 
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion(){
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+})
