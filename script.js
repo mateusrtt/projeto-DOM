6f705cfe2e90a98803062008615944ee5369b116
mateusrtt <mateuseduardo779@gmail.com>
2025-01-25 00:33:59 -0300

    Implementei evento de clique para links internos até a seção correspondente

--- script.js
+++ script.js
@@ -40,3 +40,16 @@ function initAccordion(){
   }
 }
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    console.log(section);
+}
+
+linksInternos.forEach((link) =>{
+    link.addEventListener('click', scrollToSection);
+});
