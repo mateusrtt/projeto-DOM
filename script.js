3c10aaafd556cf236c909e4797db5e946abcaf72
mateusrtt <mateuseduardo779@gmail.com>
2025-01-26 22:49:43 -0300

    Implementei animação ao rolar a página para ativar elementos com a classe '.js-scroll'

--- script.js
+++ script.js
@@ -59,3 +59,16 @@ function initScrollSuave(){
     });
 }
 initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function animaScroll(){
+    sections.forEach((section) =>{
+        const sectionTop = section.getBoundingClientRect().top;
+        if(sectionTop < 0) {
+            section.classList.add('ativo');
+        }
+    })
+}
+
+window.addEventListener('scroll', animaScroll);
